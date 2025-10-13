3e080835d9cdd809b6189bc005149d8b060a1353
Mohammed-Faisal-L <mdfaisal.a334@gmail.com>
2025-10-13 15:20:31 +0530

    make API base URL environment-aware for local and production

--- client/src/constants/api-constants.js
+++ client/src/constants/api-constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:7777";
 
 export const USER_API = {
   CREATE: `${BASE_URL}/user/createUser`,
